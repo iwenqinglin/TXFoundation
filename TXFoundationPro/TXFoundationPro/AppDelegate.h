b3a5a60d33224a763de5ee4c6ec1366cd76dd717
iwenqinglin <578622889@qq.com>
2020-04-30 00:09:03 +0800

    初始化库

---
+++ TXFoundationPro/TXFoundationPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TXFoundationPro
+//
+//  Created by wenqinglin on 2020/4/30.
+//  Copyright © 2020年 wenqinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TXFoundationPro/TXFoundationPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TXFoundationPro
+//
+//  Created by wenqinglin on 2020/4/30.
+//  Copyright © 2020年 wenqinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
