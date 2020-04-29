543cb529b76972a510c8116f789cc905d122c005
iwenqinglin <578622889@qq.com>
2020-04-30 00:17:12 +0800

    新建一个蓝牙管理类

---
+++ TXFoundation/Classes/TXCentralManager.h
@@ -0,0 +1,12 @@
+//  TXCentralManager.h
+//  TXFoundationPro
+//
+//  Created by 赵小东 on 2020/4/30.
+//  Copyright © 赵小东 (QQ:1530591908) All rights reserved.
+
+
+#import <Foundation/Foundation.h>
+
+@interface TXCentralManager : NSObject
+
+@end
---
+++ TXFoundation/Classes/TXCentralManager.m
@@ -0,0 +1,11 @@
+//  TXCentralManager.m
+//  TXFoundationPro
+//
+//  Created by 赵小东 on 2020/4/30.
+//  Copyright © 赵小东 (QQ:1530591908) All rights reserved.
+
+#import "TXCentralManager.h"
+
+@implementation TXCentralManager
+
+@end
